fa55d70758cb66665a54293d62e7ef37b0bfb7a7
Kaare Rasmussen <kaare@jasonic.dk>
2009-05-05 15:01:47 +0200

    Add a Module::Build installation.

---
+++ lib/ReportWriter.pm
@@ -0,0 +1,16 @@
+package ReportWriter;
+
+## Currently only a placeholder for $VERSION
+# POD will go here.
+# Autoloader will go here.
+##
+
+use strict;
+use warnings;
+
+use 5.010;
+
+our $VERSION   = '0.01';
+$VERSION = eval $VERSION;
+
+1;
