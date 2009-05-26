16c87efc5fa4be6509b06b90dd0afdad6ea4a0e9
Kaare Rasmussen <kaare@jasonic.dk>
2009-05-26 08:56:04 +0200

    Add Page, PDF roles for Config.

---
+++ lib/ReportWriter/Config/Role/PDF.pm
@@ -0,0 +1,17 @@
+package ReportWriter::Config::Role::PDF;
+
+use Moose::Role;
+
+with 'ReportWriter::Config::Role::Page';
+
+=pod
+
+PDF related config stuff here
+
+- sizes, positions, etc
+
+=cut
+
+no Moose::Role;
+
+1;
---
+++ lib/ReportWriter/Config/Role/Page.pm
@@ -0,0 +1,15 @@
+package ReportWriter::Config::Role::Page;
+
+use Moose::Role;
+
+=pod
+
+Page related config stuff here
+
+- page, header, footer, etc
+
+=cut
+
+no Moose::Role;
+
+1;
